13b52d770d8da026de4be55f10507d6b6fb5780a
Chidiogo <chidiogo@gmail.com>
2019-06-24 02:52:46 +0100

    signup validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+// login validation
 function validatelogin(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -10,6 +11,24 @@ function validatelogin(){
     alert("Fill in all input");
     }
 }
+
+// signup validation
+function validatesignup(){
+    var username = document.getElementById("username").value;
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    if (username && firstname && lastname && email && password){
+    alert ("signup successfully");
+    window.location = "admin.html"; 
+    return false;
+    }
+    else{
+    alert("Fill in all input");
+    }
+}
+
 
 $(document).ready(function(){
    
